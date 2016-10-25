24ef56256d1dcfbd7a5fd631d25a980d1f62f684
wekkew <wekkew@gmail.com>
2016-10-25 14:17:29 +0200

    task16 fixed

--- 02week/2day/16ex/src/16ex.cpp
+++ 02week/2day/16ex/src/16ex.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 using namespace std;
 
+
+void printA(int array[][3], int matrix) {
+	cout << "A" << endl;
+		for (int i = 0; i < matrix; i++) {
+			for (int j = 0; j < matrix; j++) {
+				cout << array[i][j];
+			}
+			cout << endl;
+		}
+}
+
+void printB(int array[][3], int matrix) {
+	cout << "B" << endl;
+	for (int i = 0; i < matrix; i++) {
+		for (int j = 0; j < matrix; j++) {
+			cout << array[j][i];
+		}
+		cout  << endl;
+	}
+}
+
+void printC(int array[][3], int matrix) {
+	cout << "C" << endl;
+		for (int i = 0; i < matrix; i++) {
+				for (int j = 0; j < matrix; j++) {
+					cout << array[i][j] << "|";
+				}
+		}
+}
+
+void printD(int array[][3], int matrix) {
+	cout << endl << "D" << endl;
+		for (int i = 0; i < matrix; i++) {
+				for (int j = 0; j < matrix; j++) {
+					cout << array[i][j] << endl;
+				}
+		}
+}
+
 int main(){
 	int m[3][3]= {{1,2,3},{4,5,6},{7,8,9}};
 	//This is an array of arrays. Print it out as a 3x3 matrix. Using each inner array as
@@ -8,33 +47,9 @@ int main(){
 	//b.) a column
 	//c.) all in one line separated by the character '|'
 	//d.) all in one column
-	cout << "A" << endl;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << m[i][j];
-		}
-		cout << endl;
-	}
-	cout << "B" << endl;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++) {
-			cout << m[j][i];
-		}
-		cout  << endl;
-	}
-	cout << "C" << endl;
-	for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				cout << m[i][j] << "|";
-			}
-	}
-	cout << endl << "D" << endl;
-	for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				cout << m[i][j] << endl;
-			}
-	}
-
-
+	printA(m, 3);
+	printB(m, 3);
+	printC(m, 3);
+	printD(m, 3);
 	return 0;
 }
