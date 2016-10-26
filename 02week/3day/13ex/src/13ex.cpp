38676a6f6958ac0eb8e4dfe29d938338637e43ac
wekkew <wekkew@gmail.com>
2016-10-26 18:27:14 +0200

    tsk 13 succeeded :)

--- 02week/3day/13ex/src/13ex.cpp
+++ 02week/3day/13ex/src/13ex.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 // Create a function that prints each word in a string into a new line!
-
 void printWords(string text) {
 	int length = text.length();
 	for (int i = 0; i < length; i++) {
@@ -13,10 +12,36 @@ void printWords(string text) {
 		}
 	}
 }
-
-
 // Create an other function that takes a string and an array of strings as it's input
 // and puts each word in the original string into a new element in the array of strings.
+int wordCounter(string text) {
+	int length = text.length();
+	int counter = 1;
+	for (int i = 0; i < length; i++) {
+		if (text[i] == ' ') {
+			counter++;
+		}
+	}
+	return counter;
+}
+
+void stringToArray(string text) {
+	string array[wordCounter(text)];
+	int arrayIndex = 0;
+	int words = wordCounter(text);
+	for (unsigned int i = 0; i < text.length(); i++) {
+		if (text[i] != ' ') {
+			array[arrayIndex] += text[i];
+		}
+		else {
+			arrayIndex++;
+			continue;
+		}
+	}
+	for (int i = 0; i < words; i++) {
+		cout << array[i] << endl;
+	}
+}
 
 // Decide which argument should be a pointer and which shouldn't.
 // In the main function, print out each element of the array into a new line!
@@ -27,10 +52,14 @@ int main(int argc, char **argv){
 	string text2 = " you should make lemonade";
 	string text3 = " and try to find somebody whose life has given them vodka";
 	string text4 = " and have a party";
-	printWords(text1);
-	printWords(text2);
-	printWords(text3);
-	printWords(text4);
+	stringToArray(text1);
+	stringToArray(text2);
+	stringToArray(text3);
+	stringToArray(text4);
+//	printWords(text1);
+//	printWords(text2);
+//	printWords(text3);
+//	printWords(text4);
 
 	return 0;
 }
