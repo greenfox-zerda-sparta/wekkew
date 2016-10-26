9bd6265725fd02916e4296f834911ef3f5d37b57
wekkew <wekkew@gmail.com>
2016-10-26 09:28:17 +0200

    task17 fixed

--- 02week/2day/17ex/src/17ex.cpp
+++ 02week/2day/17ex/src/17ex.cpp
@@ -9,15 +9,11 @@ float divide(float a, float b) {
 float reciproc(float a) {
 	return divide(1, a);
 }
-
-
 int main(int argc, char** argv){
 	//Implement the functions declared above.
 	//Maybe You can use one function in the implementation of an other one...
-
 	cout << "Dividing 1 by 4: " << divide(1, 4) << endl;
 	cout << "Dividign 1.0 by 5.0: " << divide(1.0, 5.0) << endl;
 	cout << "The repciproc of 6 is: " << reciproc(6) << endl;
-
 	return 0;
 }
