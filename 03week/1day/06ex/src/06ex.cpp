885f504a9b3e702d41ca61650537e836720c1f8c
wekkew <wekkew@gmail.com>
2016-11-02 17:36:12 +0100

    6 finished forst attempt :)

--- 03week/1day/06ex/src/06ex.cpp
+++ 03week/1day/06ex/src/06ex.cpp
@@ -1,15 +1,39 @@
-//============================================================================
-// Name        : 06ex.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+
 using namespace std;
 
+/**
+ * Create a function called "fliter_greater_than_5" that takes a pointer to an int array, and it's size.
+ * It should return a pointer to a new array that only consists the numbers that are bigger than 5.
+ */
+
+int* createArray(int size) {
+  int* array = new int[size];
+  for (int i = 0; i < size; i++) {
+    array[i] = i;
+  }
+  return array;
+}
+
+int* filterGreaterThanFive(int* array, int size) {
+  int moreThanFive;
+  for (int i = 0; i < size; i++) {
+    if (array[i] > 5) {
+      moreThanFive++;
+    }
+  }
+  int* newArray = new int[moreThanFive];
+  int newArrayIndex = 0;
+  for (int i = 0; i < size; i++) {
+    if (array[i] > 5) {
+      newArray[newArrayIndex] = array[i];
+      newArrayIndex++;
+    }
+  }
+  return newArray;
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+  cout << filterGreaterThanFive(createArray(10), 10);
+  return 0;
 }
