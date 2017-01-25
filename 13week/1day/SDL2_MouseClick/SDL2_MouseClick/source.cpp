9b0596882bcce4505b4e31cf97138d4e353f8343
Czetenyi Kristof <wekkew@gmail.com>
2017-01-25 23:30:09 +0100

    mouse event as simply as possible

--- 13week/1day/SDL2_MouseClick/SDL2_MouseClick/source.cpp
+++ 13week/1day/SDL2_MouseClick/SDL2_MouseClick/source.cpp
@@ -1,213 +1,91 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
-and may not be redistributed without written permission.*/
-
-//Using SDL, SDL_image, standard IO, and strings
-#include "LTexture.h"
-#include "LButton.h"
-
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
-//Button constants
-const int BUTTON_WIDTH = 300;
-const int BUTTON_HEIGHT = 200;
-const int TOTAL_BUTTONS = 4;
-
-enum LButtonSprite {
-	BUTTON_SPRITE_MOUSE_OUT = 0,
-	BUTTON_SPRITE_MOUSE_OVER_MOTION = 1,
-	BUTTON_SPRITE_MOUSE_DOWN = 2,
-	BUTTON_SPRITE_MOUSE_UP = 3,
-	BUTTON_SPRITE_TOTAL = 4
-};
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <string>
 
 
-//Starts up SDL and creates window
-bool init();
-
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
-
-//The window we'll be rendering to
-SDL_Window* gWindow = NULL;
-
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
-//Mouse button sprites
-SDL_Rect gSpriteClips[ BUTTON_SPRITE_TOTAL ];
-LTexture gButtonSpriteSheetTexture;
-
-//Buttons objects
-LButton gButtons[ TOTAL_BUTTONS ]; 
-
-
-
-bool init()
-{
-	//Initialization flag
-	bool success = true;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-		success = false;
-	}
-	else
-	{
-		//Set texture filtering to linear
-		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-		{
-			printf( "Warning: Linear texture filtering not enabled!" );
-		}
-
-		//Create window
-		gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow == NULL )
-		{
-			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-			success = false;
-		}
-		else
-		{
-			//Create vsynced renderer for window
-			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( gRenderer == NULL )
-			{
-				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-				success = false;
-			}
-			else
-			{
-				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-				//Initialize PNG loading
-				int imgFlags = IMG_INIT_PNG;
-				if( !( IMG_Init( imgFlags ) & imgFlags ) )
-				{
-					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
-					success = false;
-				}
-			}
-		}
-	}
-
-	return success;
+bool isInside(int x, int y, SDL_Rect* rect) {
+  if (x < rect->x) {
+    return false;
+  }
+  else if (x > rect->x + rect->w) {
+    return false;
+  }
+  else if (y < rect->y) {
+    return false;
+  }
+  else if (y > rect->y + rect->h) {
+    return false;
+  }
+  return true;
 }
 
-bool loadMedia()
-{
-	//Loading success flag
-	bool success = true;
 
-	//Load sprites
-	if( !gButtonSpriteSheetTexture.loadFromFile( "button.png" ) )
-	{
-		printf( "Failed to load button sprite texture!\n" );
-		success = false;
-	}
-	else
-	{
-		//Set sprites
-		for( int i = 0; i < BUTTON_SPRITE_TOTAL; ++i )
-		{
-			gSpriteClips[ i ].x = 0;
-			gSpriteClips[ i ].y = i * 200;
-			gSpriteClips[ i ].w = BUTTON_WIDTH;
-			gSpriteClips[ i ].h = BUTTON_HEIGHT;
-		}
 
-		//Set buttons in corners
-		gButtons[ 0 ].setPosition( 0, 0 );
-		gButtons[ 1 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, 0 );
-		gButtons[ 2 ].setPosition( 0, SCREEN_HEIGHT - BUTTON_HEIGHT );
-		gButtons[ 3 ].setPosition( SCREEN_WIDTH - BUTTON_WIDTH, SCREEN_HEIGHT - BUTTON_HEIGHT );
-	}
+int main(int argc, char* argv[]) {
+  SDL_Init(SDL_INIT_EVERYTHING);
+  IMG_Init(IMG_INIT_PNG);
+  SDL_Window* window = SDL_CreateWindow("mouseclick event", 0, 30, 640, 480, 0);
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+  
+  std::string path = "button1.png";
+  SDL_Surface* surface = IMG_Load(path.c_str());
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+  
+  SDL_Rect button = {10, 10, 100, 100};
 
-	return success;
-}
+  SDL_Event event;
+  bool running = true;
 
-void close()
-{
-	//Free loaded images
-	gButtonSpriteSheetTexture.free();
+  while (running) {
 
-	//Destroy window	
-	SDL_DestroyRenderer( gRenderer );
-	SDL_DestroyWindow( gWindow );
-	gWindow = NULL;
-	gRenderer = NULL;
+    while (SDL_PollEvent(&event)) {
+      int x, y;
+      switch (event.type) {
+      case SDL_QUIT:
+        running = false;
+        break;
+      case SDL_KEYDOWN:
+        switch (event.key.keysym.sym) {
+        case SDLK_ESCAPE:
+          running = false;
+          break;
+        case SDLK_LCTRL:
+          std::cout << "This is the left contorl button" << std::endl;
+          break;
+        }
+      case SDL_MOUSEMOTION:
+        SDL_GetMouseState(&x, &y);
+        if (isInside(x, y, &button)) {
+          std::cout << "mousemotion over \"selected\" area" << std::endl;
+        }
+        break;
+      case SDL_MOUSEBUTTONDOWN:
+        SDL_GetMouseState(&x, &y);
+        if (isInside(x, y, &button)) {
+          std::cout << "mouse button down on \"selected\" area" << std::endl;
+        }
+        break;
+      case SDL_MOUSEBUTTONUP:
+        SDL_GetMouseState(&x, &y);
+        if (isInside(x, y, &button)) {
+          std::cout << "mouse button up from \"selected\" area" << std::endl;
+        }
+        break;
+      }
+    }
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture,NULL, &button);
+    SDL_RenderPresent(renderer);
 
-	//Quit SDL subsystems
-	IMG_Quit();
-	SDL_Quit();
-}
 
-int main2( int argc, char* args[] )
-{
-	//Start up SDL and create window
-	if( !init() )
-	{
-		printf( "Failed to initialize!\n" );
-	}
-	else
-	{
-		//Load media
-		if( !loadMedia() )
-		{
-			printf( "Failed to load media!\n" );
-		}
-		else
-		{	
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
-			SDL_Event e;
-
-			//While application is running
-			while( !quit )
-			{
-				//Handle events on queue
-				while( SDL_PollEvent( &e ) != 0 )
-				{
-					//User requests quit
-					if( e.type == SDL_QUIT )
-					{
-						quit = true;
-					}
-					
-					//Handle button events
-					for( int i = 0; i < TOTAL_BUTTONS; ++i )
-					{
-						gButtons[ i ].handleEvent( &e );
-					}
-				}
-
-				//Clear screen
-				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-				SDL_RenderClear( gRenderer );
-
-				//Render buttons
-				for( int i = 0; i < TOTAL_BUTTONS; ++i )
-				{
-					gButtons[ i ].render();
-				}
-
-				//Update screen
-				SDL_RenderPresent( gRenderer );
-			}
-		}
-	}
-
-	//Free resources and close SDL
-	close();
-
-	return 0;
+  }
+  
+  IMG_Quit();
+  SDL_DestroyTexture(texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
